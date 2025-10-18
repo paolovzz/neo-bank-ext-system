968ccdeb291fcbde3cbf4cc37c9211bfe308d74f
paolovzz <paolo.vezzoso@gmail.com>
2025-10-18 12:00:19 +0200

    sviluppo

--- src/main/java/neo/bank/ext/system/adapter/output/kafka/integration_events/converters/ControlliNonSuperatiConverter.java
+++ src/main/java/neo/bank/ext/system/adapter/output/kafka/integration_events/converters/ControlliNonSuperatiConverter.java
@@ -3,7 +3,6 @@ package neo.bank.ext.system.adapter.output.kafka.integration_events.converters;
 import jakarta.enterprise.context.ApplicationScoped;
 import neo.bank.ext.system.adapter.output.kafka.integration_events.IEControlliNonSuperati;
 import neo.bank.ext.system.domain.models.events.ControlliNonSuperati;
-import neo.bank.ext.system.domain.models.events.ControlliSuperati;
 
 @ApplicationScoped
 public class ControlliNonSuperatiConverter implements IntegrationEventConverter<ControlliNonSuperati, IEControlliNonSuperati>, IntegrationEventConverterMarker{
@@ -15,8 +14,8 @@ public class ControlliNonSuperatiConverter implements IntegrationEventConverter<
 
 
     @Override
-    public Class<ControlliSuperati> supportedDomainEvent() {
-        return ControlliSuperati.class;
+    public Class<ControlliNonSuperati> supportedDomainEvent() {
+        return ControlliNonSuperati.class;
     }
     
 }
