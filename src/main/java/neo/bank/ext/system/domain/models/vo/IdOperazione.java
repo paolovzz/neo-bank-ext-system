6dfbf891811c81900ab4b5597ec49502e9cacc4c
paolovzz <paolo.vezzoso@gmail.com>
2025-11-06 14:20:48 +0100

    sviluppo

--- src/main/java/neo/bank/ext/system/domain/models/vo/IdOperazione.java
+++ src/main/java/neo/bank/ext/system/domain/models/vo/IdOperazione.java
@@ -1,14 +1,24 @@
 package neo.bank.ext.system.domain.models.vo;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import neo.bank.ext.system.domain.exceptions.ValidazioneException;
 import neo.bank.ext.system.domain.models.enums.CodiceErrore;
 
-public record IdOperazione(String id) {
-    public IdOperazione {
-        if (id == null || id.isBlank()) {
+
+@Getter
+@EqualsAndHashCode
+public class IdOperazione {
+
+    private String id;
+
+    public IdOperazione(String id) {
+        if (id == null) {
             throw new ValidazioneException(
-                    IdOperazione.class.getSimpleName(),
-                    CodiceErrore.ID_NON_PUO_ESSERE_NULL.getCodice());
+                IdOperazione.class.getSimpleName(),
+                CodiceErrore.ID_NON_PUO_ESSERE_NULL.getCodice()
+            );
         }
+        this.id = id;
     }
 }
